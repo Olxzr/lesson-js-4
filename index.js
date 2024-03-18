bb1267ebd2592c082758caa6883beda6c876eb88
olxzr <olxazar96@mail.ru>
2024-03-18 14:14:50 +0300

    1 задание сделал

---
+++ index.js
@@ -0,0 +1,8 @@
+// ? Click
+// ! 1. Напишите код, который будет реагировать на клик по кнопке и выводить в консоль сообщение "Кнопка была нажата!".
+
+const myButton = document.getElementById('myButton')
+
+myButton.addEventListener('click', () => {
+    console.log('М те1 е и кнопк');
+})
